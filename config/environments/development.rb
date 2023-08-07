a4c6df467515c56af5756c78c350e1049ae485e6
Samuel Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-08-07 15:22:30 +0000

    Add Devise to project

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,15 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Devise Mailer
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: 'your_gmail_username',
+    password: 'your_gmail_password',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
